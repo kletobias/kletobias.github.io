673d0da448bdcc1891e0e35fcf2f6541ad926d14
tobiasklein <kle.tobias@googlemail.com>
2022-05-27 19:56:37 +0200

    utilities added like fortmating guide

---
+++ script-install_jekyll-jekyllrb-site.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+ruby-install ruby
+echo "source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh" >> ~/.zshrc
+echo "source $(brew --prefix)/opt/chruby/share/chruby/auto.sh" >> ~/.zshrc
+echo "chruby ruby-3.1.1" >> ~/.zshrc
+ruby -v
+gem install jekyll
